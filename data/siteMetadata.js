136355b2f47b76791cee0aca97b49fed0f4a5fa1
hw <dusdjeks@naver.com>
2022-09-26 11:34:53 +0900

    mdx file test

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 const siteMetadata = {
-  title: `Woo's Log`,
+  title: `나현우 개인 블로그`,
   author: 'Hyunwoo',
   headerTitle: `Woo's Log`,
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://www.wooslog.com/',
+  siteRepo: 'https://github.com/Na-hyunwoo/woos-log',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
